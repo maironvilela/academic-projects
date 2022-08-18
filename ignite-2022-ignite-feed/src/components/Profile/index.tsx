2d0d8bfd12a2bc988bdfc00bcbb0cd3ffe3a6d7a
Mairon Vilela <maironvilela@hotmail.com>
2022-08-18 05:16:22 -0300

    feat: add profile component

---
+++ ignite-2022-ignite-feed/src/components/Profile/index.tsx
@@ -0,0 +1,26 @@
+import { faker } from '@faker-js/faker';
+import { PencilLine } from 'phosphor-react';
+import styles from './styles.module.css';
+export function Profile() {
+  return (
+    <aside className={styles.aside}>
+      <figure className={styles.cover}>
+        <img src={faker.image.city()} />
+      </figure>
+
+      <figure className={styles.avatar}>
+        <img src={faker.internet.avatar()} />
+      </figure>
+
+      <strong>Mairon Vilela</strong>
+      <span>Developer</span>
+
+      <footer className={styles.footer}>
+        <a href="#">
+          <PencilLine size={20} />
+          <span>Editar seu perfil</span>
+        </a>
+      </footer>
+    </aside>
+  );
+}
