ad700a9561a19376b90d876531c54cf303a51e51
Mairon Vilela <maironvilela@hotmail.com>
2022-09-18 20:53:47 -0300

    feat: [Ignite-feed] add feed component layout

--- ignite-2022-ignite-feed/src/App.tsx
+++ ignite-2022-ignite-feed/src/App.tsx
@@ -1,11 +1,15 @@
-import { Header, Profile } from './components';
+import { Feed, Header, Profile } from './components';
+import styles from './styles/app.module.css';
 import './styles/global.css';
 
 function App() {
   return (
     <>
       <Header />
-      <Profile />
+      <div className={styles.content}>
+        <Profile />
+        <Feed />
+      </div>
     </>
   );
 }
---
+++ ignite-2022-ignite-feed/src/components/Post/index.tsx
@@ -0,0 +1,48 @@
+import { faker } from '@faker-js/faker';
+import styles from './styles.module.css';
+
+export function Feed() {
+  return (
+    <article className={styles.container}>
+      <header className={styles.header}>
+        <figure className={styles.avatar}>
+          <img src={faker.internet.avatar()} />
+        </figure>
+        <section className={styles.authorInfo}>
+          <strong>{faker.name.fullName()}</strong>
+          <span>{faker.name.jobArea()}</span>
+        </section>
+        <time
+          dateTime={`${faker.date.recent()}`}
+          title={`${faker.date.past().toDateString()}`}
+        >
+          Públicado há 1h
+        </time>
+      </header>
+
+      <section className={styles.main}>
+        <p>Fala Galera 👏</p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+
+        <a href="#"> 👉 jane.design/doctorcare</a>
+
+        <p className={styles.tags}>
+          <a href="#"> #novoprojeto </a>
+          <a href="#"> #nlw </a>
+          <a href="#"> #rocketseat</a>
+        </p>
+      </section>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea></textarea>
+        <footer>
+          <button> Publicar </button>
+        </footer>
+      </form>
+    </article>
+  );
+}
