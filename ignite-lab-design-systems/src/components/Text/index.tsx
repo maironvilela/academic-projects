f653a6926647e8abe33794d95e226a83188e4b13
Mairon Vilela <maironvilela@hotmail.com>
2022-10-21 00:02:35 -0300

    feat: [ignite-lab-design-system] Text component

--- ignite-lab-design-systems/src/components/Text/index.tsx
+++ ignite-lab-design-systems/src/components/Text/index.tsx
@@ -1,3 +1,23 @@
-export function Text (){
-    return <span className="text-gray-100">Text</span>;
+import { Slot } from '@radix-ui/react-slot';
+import { clsx } from 'clsx';
+import { ReactNode } from 'react';
+
+
+
+export type TextProps = {
+  size?: 'sm'|'md'|'lg',
+  children: ReactNode
+  asChild: boolean
+}
+
+export function Text ({size = 'md', children, asChild}:TextProps){
+    const Comp = asChild ? Slot : 'span';
+
+    return (
+        <Comp className={clsx('text-gray-100', {
+            'text-xs': size === 'sm',
+            'text-sm': size === 'md',
+            'text-md': size === 'lg',
+        })} >{children}</Comp>
+    );
 }
