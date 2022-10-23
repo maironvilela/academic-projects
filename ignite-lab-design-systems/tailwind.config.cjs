e9295a9fc2b65b2bc89559fc02b233f94e942d1e
Mairon Vilela <maironvilela@hotmail.com>
2022-10-22 23:45:37 -0300

    feat: [ignite-lab-design-system]  InputText component implementation

--- ignite-lab-design-systems/tailwind.config.cjs
+++ ignite-lab-design-systems/tailwind.config.cjs
@@ -12,19 +12,23 @@ module.exports = {
             '2xl': 32
         },
         colors:{
-            'black': '#000000',
-            'white': '#FFFFFF',
+            transparent: 'transparent',
+            black: '#000000',
+            white: '#FFFFFF',
 
-            'gray-100': '#E1E1E6',
-            'gray-200': '#C4C4CC',
-            'gray-500': '#7C7C7C',
-            'gray-600': '#7C7C8A',
-            'gray-800': '#202024',
-            'gray-900': '#1A1A1A',
+            gray:{
+                100: '#E1E1E6',
+                200: '#C4C4CC',
+                500: '#7C7C7C',
+                600: '#7C7C8A',
+                800: '#202024',
+                900: '#1A1A1A'
+            },
 
-            'cyan-100': '#93DDF1',
-            'cyan-300': '#61DAFB',
-
+            cyan:{
+                100: '#93DDF1',
+                300: '#61DAFB',
+            }
         },
         extend: {
             fontFamily:{
