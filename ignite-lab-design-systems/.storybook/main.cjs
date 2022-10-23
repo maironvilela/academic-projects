71a842cd071b286856f2d7f5002f88e0686b9ab4
Mairon Vilela <maironvilela@hotmail.com>
2022-10-23 16:51:59 -0300

    feat: [ignite-lab-design-system]  CI storybook

--- ignite-lab-design-systems/.storybook/main.cjs
+++ ignite-lab-design-systems/.storybook/main.cjs
@@ -13,5 +13,12 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/academic-projects/'
+    }
+
+    return config
   }
 }
